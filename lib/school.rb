8ef7522d7cecb3f50f04d4d15d257b8050743548
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-19 13:13:36 -0600

    Adds end time method

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,10 @@ class School
   end
 
   def add_student_name(name)
-    @student_names << name 
+    @student_names << name
   end
 
+  def end_time
+    (@start_time.to_i + @hours_in_school_day).to_s + ":00"
+  end
 end
